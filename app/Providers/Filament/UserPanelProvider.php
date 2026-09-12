e2eca3f8793003031c0643c8bf2282c1dfcae951
elsmrsoufiane <soufianeasmar0@gmail.com>
2026-09-12 16:49:47 +0100

    removing user dashboard

--- app/Providers/Filament/UserPanelProvider.php
+++ app/Providers/Filament/UserPanelProvider.php
@@ -36,7 +36,6 @@ class UserPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/User/Resources'), for: 'App\Filament\User\Resources')
             ->discoverPages(in: app_path('Filament/User/Pages'), for: 'App\Filament\User\Pages')
             ->pages([
-                Dashboard::class,
                 Chat::class,
             ])
             ->discoverWidgets(in: app_path('Filament/User/Widgets'), for: 'App\Filament\User\Widgets')
